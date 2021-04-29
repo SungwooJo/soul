69c7dec6271faef69656169644683bb1e6dc1196
SungwooJo <jjo6734@gmail.com>
2021-04-29 23:55:39 +0900

    ENH: update gitignore

--- src/Sky/Star.tsx
+++ src/Sky/Star.tsx
@@ -1,8 +1,8 @@
 import styled from "styled-components/macro";
 
-const Atom = styled.div`
+const Star = styled.div`
   width: 8px;
   height: 8px;
 `;
 
-export default Atom;
+export default Star;
--- src/Sky/index.tsx
+++ src/Sky/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components/macro";
 
-import Atom from "./Atom";
+import Star from "./Star";
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -12,7 +12,7 @@ const Wrapper = styled.div`
 function Wave() {
   return (
     <Wrapper>
-      <Atom />
+      <Star />
     </Wrapper>
   );
 }
